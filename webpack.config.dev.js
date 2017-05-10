87c92d43c21dd4fd7ed51178b7fbf1cb71cf6ce9
sundnnstore <sundnnstore@gmail.com>
2017-05-10 17:42:46 +0800

    new updated

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,18 +1,28 @@
 import path from 'path';
+import webpack from 'webpack';
+
 export default{
      devtool: 'eval-source-map',
-entry:path.join(__dirname,'/client/index.js'),
+ entry: [
+    'webpack-hot-middleware/client',
+    path.join(__dirname, '/client/index.js')
+  ],
 output:{
-    path: path.join(__dirname, 'server'),
+   path: '/',
     filename: 'bundle.js',
     publicPath: '/'
 },
+ plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
 module:{
     loaders:[
         {
 test:/\.js$/,
 include:path.join(__dirname,'client'),
-loaders:['babel-loader']
+loaders:['react-hot-loader','babel-loader']
         }
     ]
 },
